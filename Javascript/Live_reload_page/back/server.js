debb8681df6c290d17c857d2617f83180462c5e2
rjmata95 <12-11030@usb.ve>
2021-03-16 17:02:05 -0400

    Ready for deploy using Heroku

--- Javascript/Live_reload_page/back/server.js
+++ Javascript/Live_reload_page/back/server.js
@@ -2,9 +2,14 @@ const express = require('express')
 const { ExpectationFailed } = require('http-errors')
 
 var app = express();
+let port = process.env.PORT || 3000;
 
 // app.use('/', express.static('./front/src/index.html'))
 app.use('/', express.static('front/src'))
 
-app.listen(3000)
+
+// if (port == null || port == ""){
+//     port = 3000;
+// }
+app.listen(port)
 console.log('Listening on http://localhost:3000/')
