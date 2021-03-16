816e8d3b80c5499b7981a2ff556111e111ab3c8c
rjmata95 <12-11030@usb.ve>
2021-03-16 14:50:16 -0400

    added server features using express

--- Javascript/Live_reload_page/front/src/index.js
+++ Javascript/Live_reload_page/front/src/index.js
@@ -3,12 +3,12 @@ $(() => {
     var $form = $('form')
     var $displayUsername = $('#display-username')
     var $displayComment = $('#display-comment')
-    var a = $( '<h1>', {
-        style: 'background-color:rgb(0,0,255)',
-        html: 'Prueba de jQuery'
-    })
+    // var a = $( '<h1>', {
+    //     style: 'background-color:rgb(0,0,255)',
+    //     html: 'Prueba de jQuery'
+    // })
 
-    $main.append(a)
+    // $main.append(a)
     
     $form.on('submit', (ev) => {
         
